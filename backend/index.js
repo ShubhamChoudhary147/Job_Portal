71aeb07ccd4451a4a78c957658238e26a896e38b
Shubham Choudhary <shubhamchoudhary.030704@gmail.com>
2025-05-15 17:33:44 +0530

    changes in index.js file

--- backend/index.js
+++ backend/index.js
@@ -16,12 +16,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+
+const allowedOrigins = [
+  "https://jobhunntt.vercel.app",
+  "http://localhost:5173"
+];
+
 const corsOptions = {
-    origin: [ 
-        'http://localhost:5173',                    
-        'https://https://jobhunntt.vercel.app/'          
-    ],
-    credentials:true
+    origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = "The CORS policy for this site does not allow access from the specified Origin.";
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true,
 }
 
 app.use(cors(corsOptions));
